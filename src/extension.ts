72809a44d90fe7b715f69196c861bbadf979bafb
Josa Gesell <josa@gesell.me>
2019-10-29 16:21:39 +0100

    Install language server as dependency again

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,21 @@
 import path from 'path'
+import fs from 'fs'
+import pkgDir from 'pkg-dir'
 import { TransportKind, ExtensionContext, LanguageClient, ServerOptions, commands, workspace, services, LanguageClientOptions } from 'coc.nvim'
-import { pkgBin, commandExists, pkgInstall, pkgUpgrade } from './utils'
+
+interface ShConfig {
+  enable: boolean
+  commandPath: string
+}
 
 export async function activate(context: ExtensionContext): Promise<void> {
-
-  const config = workspace.getConfiguration().get('sh', {}) as any
+  const config = workspace.getConfiguration().get('sh', {}) as ShConfig
   if (config.enable === false) {
     return
   }
-  const command = config.commandPath || await pkgBin('bash-language-server')
-  if (!await commandExists(command)) {
-    await pkgInstall('bash-language-server')
-  }
 
   const serverOptions: ServerOptions = {
-    command,
+    command: (config.commandPath || await pkgBin('bash-language-server')),
     args: ['start'],
     transport: TransportKind.stdio
   }
@@ -23,17 +24,27 @@ export async function activate(context: ExtensionContext): Promise<void> {
     documentSelector: ['sh']
   }
 
-  const client = new LanguageClient( 'sh', 'bash-language-server', serverOptions, clientOptions)
+  const client = new LanguageClient('sh', 'bash-language-server', serverOptions, clientOptions)
 
   context.subscriptions.push(
     services.registLanguageClient(client),
     commands.registerCommand("sh.version", async () => {
-      const v = require(path.resolve(__dirname, '..', 'package.json')).version
-      workspace.showMessage(`Version: ${v}`, 'more')
-    }),
-    commands.registerCommand("sh.update.bash-language-server", async () => {
-      await pkgUpgrade('bash-language-server')
+      const rootDir = await pkgDir(__dirname)
+      const v = require(path.resolve(rootDir, 'package.json')).version
+      workspace.showMessage(`Version: ${v} [node: ${process.versions.node}]`, 'more')
     })
   )
+}
+
+async function pkgBin(name: string): Promise<string> {
+  const rootDir = await pkgDir(__dirname)
+  let bin = path.join(rootDir, 'node_modules', '.bin', name)
+  try {
+    bin = fs.realpathSync(bin)
+  } catch (e) {
+    // ignore
+  }
+
+  return bin
 }
 
